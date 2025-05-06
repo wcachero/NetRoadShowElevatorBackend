6a0aede3ee24756e2af1a37c991b37ded6733211
wcachero <wilson.09c@gmail.com>
2025-05-06 15:12:53 +0800

    Fix commit

---
+++ NetRoadShowElevatorTest/Services/BackgroundServices/ElevatorWorkerServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ElevatorMovement.Services.BackgroundServices;
+using ElevatorMovement.Services;
+using FakeItEasy;
+using Xunit;
+
+namespace NetRoadShowElevatorTest.Services.BackgroundServices
+{
+    public class ElevatorWorkerServiceTest
+    {
+        [Fact]
+        public async Task ExecuteAsync_ShouldCallStepMethod()
+        {
+            // Arrange
+            var elevatorSystemService = A.Fake<ElevatorSystemService>();
+            var workerService = new ElevatorWorkerService(elevatorSystemService);
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            // Act
+            var executeTask = workerService.StartAsync(cancellationTokenSource.Token);
+
+            // Allow the service to run briefly
+            await Task.Delay(50);
+
+            // Stop the service
+            cancellationTokenSource.Cancel();
+            await executeTask;
+
+            // Assert
+
+             A.CallTo(() => elevatorSystemService.Step()).MustHaveHappened();
+ 
+        }
+    }
+}
